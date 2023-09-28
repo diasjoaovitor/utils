b6a707fe1802e5143674730f2373bba1a89d392b
João Vitor <jvitordiass@outlook.com>
2023-09-27 23:14:42 -0300

    refactor: implements EAN-13 algorithm

--- src/pages/cod-barras/script.js
+++ src/pages/cod-barras/script.js
@@ -2,21 +2,16 @@ const app = document.getElementById('codigoDeBarras')
 const input = app.querySelector('input')
 const output = app.querySelector('output')
 
-function separarCodigoDeBarras(codigoDeBarras) {
-  const primeiroDigito = codigoDeBarras.slice(0, 1)
-  const ultimoDigito = codigoDeBarras.slice(-1)
-  const meio = codigoDeBarras.slice(1, codigoDeBarras.length - 1)
-  return { primeiroDigito, ultimoDigito, meio }
+function calcularCodigoDeBarrasDaUnidade(codigoDeBarrasDaEmbalagem) {
+  const array = codigoDeBarrasDaEmbalagem.slice(1, -1).split('')
+  const soma = array.reduce((total, numero, i) => total += i % 2 === 0 ? numero * 1 : numero * 3, 0)
+  const ultimoDigito = (Math.floor(soma / 10) + 1) * 10 - soma
+  const codigo = array.join('') + ultimoDigito
+  return codigo
 }
 
-function calcularCodigoDeBarras(primeiroDigito, ultimoDigito, meio) {
-  const somador = primeiroDigito * 3
-  const novoUltimoDigito = (Number(ultimoDigito) + somador) % 10
-  return meio + novoUltimoDigito
-}
-
-function copiarCodigo(codigoDeBarras) {
-  navigator.clipboard.writeText(codigoDeBarras)
+function copiarCodigo(codigo) {
+  navigator.clipboard.writeText(codigo)
 }
 
 function notificarCodigoCopiado() {
@@ -34,16 +29,11 @@ function render(content) {
 input.addEventListener('change', e => {
   const codigoDeBarrasDaEmbalagem = e.target.value
   if (codigoDeBarrasDaEmbalagem.length !== 14) {
-    render('O código de barras deve conter 14 dígitos')
+    render('O código de barras da embalagem deve conter 14 dígitos')
     return
   }
-  const { primeiroDigito, ultimoDigito, meio } = separarCodigoDeBarras(codigoDeBarrasDaEmbalagem)
-  if (primeiroDigito < 0) {
-    render('Validação não implementada! confira se existe o caso nas tabelas')
-    return
-  }
-  const codigoDeBarras = calcularCodigoDeBarras(primeiroDigito, ultimoDigito, meio)
-  render(codigoDeBarras)
-  copiarCodigo(codigoDeBarras)
+  const codigoDeBarrasDaUnidade = calcularCodigoDeBarrasDaUnidade(codigoDeBarrasDaEmbalagem)
+  render(codigoDeBarrasDaUnidade)
+  copiarCodigo(codigoDeBarrasDaUnidade)
   notificarCodigoCopiado()
 })
